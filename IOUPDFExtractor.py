96ee3c3116410cce9d02c4d59a363cf7d9180304
henrique <henriquealmeida1837@gmail.com>
2020-03-18 19:36:00 -0300

    primeira versao

--- IOUPDFExtractor.py
+++ IOUPDFExtractor.py
@@ -10,20 +10,44 @@ Created on Wed Mar 18 17:48:10 2020
 import PyPDF2
 import pandas as pd
 
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
 # creating an object 
-file = open('/home/henrique/Downloads/DIC07_20_03_0022_.pdf', 'rb')
+file = open('/home/henrique/Downloads/DIC07_20_03_0021_.pdf', 'rb')
 
 
 # creating a pdf reader object
 fileReader = PyPDF2.PdfFileReader(file)
 
 
-pageObj = fileReader.getPage(0)
-p1_page = pageObj.extractText().split("Data File",1)[0].split("|-------|----|-------|----------|----------|--------|",1)[1]
-pageObj = fileReader.getPage(1)
-p2_page = pageObj.extractText().split("Totals :",1)[0].split("|-------|----|-------|----------|----------|--------|",1)[1]
+totalPages = fileReader.getNumPages()
+pages = []
 
-table_arr = (p1_page + p2_page).split()
+for i in range(totalPages):
+    pageObj = fileReader.getPage(i)
+    if(i < totalPages -1):
+        pages.append(pageObj.extractText().split("Data File",1)[0].split("|-------|----|-------|----------|----------|--------|",1)[1])
+    else:
+        pages.append(pageObj.extractText().split("Totals :",1)[0].split("|-------|----|-------|----------|----------|--------|",1)[1])
+
+table_arr = ''.join(pages).split()
+
+treated_table_arr = []
+i = 0
+while i < len(table_arr):
+    if(isfloat(table_arr[i])==False and isfloat(table_arr[i+1])==False):
+        treated_table_arr.append(table_arr[i]+" "+table_arr[i+1])
+        i = i + 2
+    else:
+        treated_table_arr.append(table_arr[i])
+        i = i + 1
+    
 columns = 7
 i = 0
 df = pd.DataFrame({
@@ -35,14 +59,14 @@ df = pd.DataFrame({
     "height": [],
     "area_p": []
 })
-while i < len(table_arr):
-    peak = table_arr[i]
-    ret_time = table_arr[i]
-    width = table_arr[i+1]
-    typ = table_arr[i+2]
-    area = table_arr[i+3]
-    height = table_arr[i+4] 
-    area_p = table_arr[i+5]
+while i < len(treated_table_arr):
+    peak = treated_table_arr[i]
+    ret_time = treated_table_arr[i+1]
+    typ = treated_table_arr[i+2]
+    width = treated_table_arr[i+3]
+    area = treated_table_arr[i+4]
+    height = treated_table_arr[i+5] 
+    area_p = treated_table_arr[i+6]
     i = i + columns
     df = df.append({
         "peak": peak,
@@ -55,4 +79,4 @@ while i < len(table_arr):
     },ignore_index=True)
 # print the number of pages in pdf file
     
-df.to_csv("/home/henrique/Downloads/iou.csv",sep=";")
+df.to_csv("/home/henrique/Downloads/iou_2.csv",sep=";")
